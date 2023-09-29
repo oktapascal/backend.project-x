75c1e535b80b1873c38bd5e2260651233da76ec2
oktapascal <oktaiscool@gmail.com>
2023-09-29 16:08:38 +0700

    Feat: Create module menus controller

---
+++ src/modules/modules-menu.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Get, HttpCode, HttpStatus, Inject, Param, Post } from '@nestjs/common';
+import { MODULES_MENU_SERVICES, ModulesMenuServices } from './modules-menu.service';
+import { CreateModuleMenuRequest } from './request/create-module-menu.request';
+
+@Controller('modules-menu')
+export class ModulesMenuController {
+  constructor(@Inject(MODULES_MENU_SERVICES) private readonly services: ModulesMenuServices) {}
+
+  @Post('/')
+  @HttpCode(HttpStatus.CREATED)
+  async SaveMenus(@Body() request: CreateModuleMenuRequest) {
+    await this.services.SaveModulesMenu(request);
+
+    return {
+      code: HttpStatus.OK,
+      status: 'OK',
+    };
+  }
+
+  @Get('/:module_id')
+  @HttpCode(HttpStatus.OK)
+  GetMenus(@Param('module_id') module_id: string) {
+    return this.services.GetModuleMenus(module_id);
+  }
+}
--- src/modules/modules.controller.ts
+++ src/modules/modules.controller.ts
@@ -7,31 +7,31 @@ import { CreateModuleRequest, UpdateModuleRequest } from './request';
 
 @Controller('module')
 export class ModulesController {
-  constructor(@Inject(MODULES_SERVICES) private readonly modulesServices: ModulesServices) {}
+  constructor(@Inject(MODULES_SERVICES) private readonly services: ModulesServices) {}
 
   @Get('/user')
   @HttpCode(HttpStatus.OK)
   @Serialize(ModulesDto)
   GetModulesByUser(@CurrentUser() user: Express.User) {
-    return this.modulesServices.GetModulesByUser(user['sub']);
+    return this.services.GetModulesByUser(user['sub']);
   }
 
   @Post('/')
   @HttpCode(HttpStatus.CREATED)
   SaveModule(@Body() request: CreateModuleRequest) {
-    return this.modulesServices.SaveModule(request);
+    return this.services.SaveModule(request);
   }
 
   @Patch('/')
   @HttpCode(HttpStatus.OK)
   UpdateModule(@Body() request: UpdateModuleRequest) {
-    return this.modulesServices.UpdateModule(request);
+    return this.services.UpdateModule(request);
   }
 
   @Delete('/:module_id')
   @HttpCode(HttpStatus.OK)
   async DeleteModule(@Param('module_id') module_id: string) {
-    await this.modulesServices.DeleteModule(module_id);
+    await this.services.DeleteModule(module_id);
 
     return {
       code: HttpStatus.OK,
