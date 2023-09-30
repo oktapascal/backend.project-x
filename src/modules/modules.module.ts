a346f12e994271f2855791004d9dfe07adc27ba0
oktapascal <oktaiscool@gmail.com>
2023-09-30 11:20:06 +0700

    update

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -8,13 +8,14 @@ import { ModulesRoleController } from './modules-role.controller';
 import { MODULES_ROLE_SERVICES, ModulesRoleServiceImpl } from './modules-role.service';
 import { MODULES_MENU_REPOSITORIES, ModulesMenuRepositoriesImpl } from './modules-menu.repositories';
 import { MODULES_MENU_SERVICES, ModulesMenuServicesImpl } from './modules-menu.service';
+import { MODULES_ROLE_REPOSITORIES, ModulesRoleRepositoriesImpl } from './modules-role.repositories';
 
 @Module({
   controllers: [ModulesController, ModulesRoleController],
   providers: [
     { provide: MODULES_REPOSITORIES, useClass: ModulesRepositoriesImpl },
     { provide: MODULES_SERVICES, useClass: ModulesServicesImpl },
-    { provide: MODULES_REPOSITORIES, useClass: ModulesRepositoriesImpl },
+    { provide: MODULES_ROLE_REPOSITORIES, useClass: ModulesRoleRepositoriesImpl },
     { provide: MODULES_ROLE_SERVICES, useClass: ModulesRoleServiceImpl },
     { provide: MODULES_MENU_REPOSITORIES, useClass: ModulesMenuRepositoriesImpl },
     { provide: MODULES_MENU_SERVICES, useClass: ModulesMenuServicesImpl },
